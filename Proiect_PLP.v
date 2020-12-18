d6f1e3a56c5a7516904e4b184adcfea6ea997d31
cristinaaa272 <75524144+cristinaaa272@users.noreply.github.com>
2020-12-18 17:12:24 +0200

    Add files via upload

--- Proiect_PLP.v
+++ Proiect_PLP.v
@@ -29,15 +29,15 @@ Inductive ErrString :=
 Inductive ErrPointer :=
  | err_pointer : ErrPointer
  | NULL : ErrPointer
- | nat_pointer : nat -> ErrPointer
- | bool_pointer : nat -> ErrPointer
- | str_pointer : nat -> ErrPointer.
+ | pointer : string -> ErrPointer
+ | reference : string -> ErrPointer .
 
-
+Scheme Equality for ErrPointer.
 
 Coercion num: nat >-> ErrNat.
 Coercion boolean: bool >-> ErrBool.
 Coercion str: string >-> ErrString.
+
 
 
 Check 7.
@@ -49,7 +49,6 @@ Check "x".
 Inductive AExp :=
 | avar: string -> AExp 
 | anum: ErrNat -> AExp
-| ptr_nat : string -> AExp
 | aplus: AExp -> AExp -> AExp
 | aminus: AExp -> AExp -> AExp
 | amul: AExp -> AExp -> AExp 
@@ -125,7 +124,6 @@ Inductive BExp :=
 | b_true : BExp
 | b_false : BExp
 | b_var: string -> BExp 
-| ptr_bool : string -> BExp
 | b_less : AExp -> AExp -> BExp
 | b_more : AExp -> AExp -> BExp
 | b_less_eq : AExp -> AExp -> BExp
@@ -213,7 +211,6 @@ Definition xorb_err (b1 b2 : ErrBool) : ErrBool :=
 Inductive STRexp :=
  | str_err : ErrString -> STRexp
  | str_var : string -> STRexp
- | ptr_string : string -> STRexp
  | strcmp : ErrString -> ErrString -> STRexp
  | strcat : ErrString -> ErrString -> STRexp.
 
@@ -264,8 +261,8 @@ Inductive ErrArray :=
 
 Inductive Array_opp :=
  | arr_len : ErrArray -> Array_opp
- | insert : ErrArray -> Array_opp
- | delete : ErrArray -> Array_opp
+ | elem : string -> nat -> Array_opp
+ | delete : string -> nat -> Array_opp
  | min : ErrArray -> Array_opp
  | max : ErrArray -> Array_opp.
 
@@ -275,20 +272,20 @@ Inductive Array_opp :=
 
 Inductive Statement :=
   | nat_decl: string -> AExp -> Statement 
-  | ptr_nat_decl : string -> string -> Statement
   | bool_decl: string -> BExp -> Statement 
-  | ptr_bool_decl : string -> string -> Statement
   | str_decl : string -> STRexp -> Statement
+  | array_decl_n : string -> nat -> (list nat) -> Statement
+  | array_decl_b : string -> nat  -> (list bool) -> Statement
+  | array_decl_s : string -> nat -> (list string) -> Statement
+  | ptr_nat_decl : string -> string -> Statement
+  | ptr_bool_decl : string -> string -> Statement
   | ptr_string_decl : string -> string -> Statement
-  | array_decl_n : string -> nat -> Statement
-  | array_decl_b : string -> nat  -> Statement
-  | array_decl_s : string -> nat -> Statement
+  | ref_decl : string -> string -> Statement
   | nat_assign : string -> AExp -> Statement 
-  | ptr_nat_assign : string -> AExp -> Statement 
   | bool_assign : string -> BExp -> Statement
-  | ptr_bool_assign : string -> BExp -> Statement
   | str_assign : string -> string -> Statement
-  | ptr_string_assign : string -> STRexp -> Statement
+  | ptr_assign : string -> string -> Statement
+  | ref_assign : string -> string -> Statement
   | array_assign_n : string -> nat -> (list nat) -> Statement
   | array_assign_b : string -> nat -> (list bool) -> Statement
   | array_assign_s : string -> nat -> (list string) -> Statement 
@@ -305,10 +302,7 @@ Inductive Statement :=
   | fun_call : string -> (list string) -> Statement (*apel de functie cu o serie de parametrii *)
    with Case :=
     | case_default : Statement -> Case
-    | case_x : AExp -> Statement -> Case .
-
-
-
+    | case : AExp -> Statement -> Case.
 
 
 
@@ -333,13 +327,114 @@ Inductive Pgm :=
 | default_nat_decl : string -> Pgm (*declarare nat cu valoare default*)
 | default_bool_decl : string -> Pgm (*declarare int cu valoare default*)
 | default_string_decl : string -> Pgm (*declarare string cu valoare default*)
-| default_array_decl : string -> Pgm
+| default_array_decl : string -> nat -> Pgm
+| default_ptr_decl : string -> Pgm
 | main : Statement -> Pgm (* main function - no parameters *)
 | function : string -> list string -> Statement -> Pgm. (* functions *)
 
 
 
 (* Default values types - used for variables of different types outside a function *)
+
+(*---> SEMANTICA*)
+
+
+Definition type_equality  (t1 : Types)(t2 : Types) : bool :=
+ match t1 with 
+ | err_undecl => match t2 with 
+                 | err_undecl => true
+                 | _ => false
+                end
+ | err_assign => match t2 with 
+                 | err_assign => true
+                 | _ => false
+                end
+ | default => match t2 with 
+                 | default => true
+                 | _ => false
+                end
+ | val_nat a => match t2 with
+              | val_nat b => true
+              | _ => false
+              end
+ | val_bool b1 => match t2 with
+              | val_bool b2 => true
+              | _ => false
+              end
+ | val_string s1 => match t2 with
+              | val_string s2 => true
+              | _ => false
+              end
+ | val_pointer p1 => match t2 with
+                    | val_pointer p2 => true
+                    | _ => false
+                     end
+ | val_array v1 => match t2 with
+                   | val_array v2 => true
+                   | _ => false
+                   end
+ | code _x => match t2 with
+                  | code _x => true
+                  | _ => false
+                 end
+  end.
+
+Compute (type_equality err_undecl err_assign ).
+Compute (type_equality (val_nat 100) (val_bool false)).
+Compute (type_equality (val_string "s1") (val_string "s2")).
+
+(*Configuration for functions : <Env, Memory, Stack> *)
+
+Inductive Memory :=
+  | default_mem : Memory
+  | offset : nat -> Memory.
+
+Scheme Equality for Memory.
+
+Definition Env := string -> Memory. (* mapare nume_varialiba -> zona de memorie*)
+
+Definition MemLayer := Memory -> Types. (*mapare zona_memorie -> un tip anume indicat *)
+
+Definition Stack := list Env. (*lista de env-uri ce se actualizeaza pe parcursul rularii programului*)
+
+Inductive Config :=
+  | config : nat -> Env -> MemLayer -> Stack -> Config.
+ (* current memory zone -> environment -> memory layer -> stack *)
+
+Definition update_env (env : Env) (v : string) (m : Memory) : Env :=
+  fun y =>
+      if (andb (string_beq v y ) (Memory_beq (env y) default_mem))
+      then
+        m
+      else
+        (env y).
+Notation "S [ V // X ]" := (update_env S X V) (at level 0).
+
+(*Daca variabila nu are nici macar default_mem  , returnam env 
+ Daca variabila are default_mem / altceva , actualizam cu noul offset la care se afla variabila *)
+
+
+Definition env : Env := fun x => default_mem. (*env initial *)
+Compute (env "var"). (*"var" nu are asignata inca o zona de memorie *)
+Compute (update_env env "var" (offset 5)) "var". (*ii schimbam variabilei "var" offset-ul la 5*)
+
+Definition update_mem (mem : MemLayer) (env : Env) (x : string) (type : Memory) (v : Types) : MemLayer :=
+  fun y => 
+      if(andb (Memory_beq (env x) type) (Memory_beq y type))
+      then
+        if(andb(type_equality err_undecl (mem y)) (negb(type_equality default v)))
+        then err_undecl
+        else if (type_equality err_undecl (mem y))
+            then default
+            else if(orb(type_equality default (mem y)) (type_equality v (mem y)))
+                 then v
+                 else err_assign
+      else (mem y).
+
+Notation "S [ V /m/ X ]" := (update_mem S X V) (at level 0).
+
+Definition mem : MemLayer := fun x => err_undecl.
+
 
 (*Notations & examples for arithmetic expressions *)
 Notation "A +' B" := (aplus A B)(at level 50, left associativity).
@@ -349,15 +444,12 @@ Notation "A /' B" := (adiv A B)(at level 48, left associativity).
 Notation "A %' B" := (amod A B)(at level 45, left associativity).
 Notation "A +++" := (ainc A) (at level 40,left associativity).
 Notation "A ---" := (adec A) (at level 49). 
-Notation "'NAT*' X" := (ptr_nat X) (at level 0).
-
 
 Check (2 +' 3 *' 5).
 Check (2 +' 3 *' "n").
 Check ("sum" /' "sum"---).
 Check ("i" +++).
 Check (3 %' 0).
-Check ( NAT* "i").
 
 (*Notations & examples for boolean expressions *)
 Notation "A <' B" := (b_less A B) (at level 53).
@@ -369,7 +461,6 @@ Notation " ! A " := (b_not A) (at level 71).
 Notation "A & B" := (b_and A B) (at level 75).
 Notation "A | B" := (b_or A B) (at level 75).
 Notation "A ^^ B" := (b_xor A B) (at level 60).
-Notation "'BOOL*' X" := (ptr_bool X) (at level 0).
 
 Check ( 3 >=' ("x" /' 0)) .
 Check (( ("a" +' 5) /' (("b" -'1) *' 3)) <=' 100 ).
@@ -377,19 +468,17 @@ Check (( ! b_false ) & b_true).
 Check ( b_true | b_false | "sum" | ("a" <=' "b")).
 Check (b_true ^^ b_false).
 Check ("a" ^^ "b").
-Check ( BOOL* "bool").
 
 
 (*Notations & examples for string operations *)
 Notation " len[ S ] " := (strlen S) (at level 31).
 Notation " S1 /+/ S2 " := (strcat S1 S2) (at level 30).
 Notation " S1 ? S2 " := (strcmp S1 S2) (at level 32).
-Notation "'STR*' X" := (ptr_string X) (at level 0).
+
 
 Check ("Info " /+/ "PLP") .
 Check (len[ "Proiect" ]).
 Check ("ab" ? "ba") .
-Check ( STR* "string").
 
 
 (*Notations & examples for arrays*)
@@ -403,49 +492,59 @@ Check [].
 Check [true , false].
 Check ["proiect" , "PLP"].
 
-Notation "'Nat_array' A '[(' X ')]' " :=(array_decl_n A X)(at level 4).
+Notation "'Nat_array' A '[(' X ')]' -> L " :=(array_decl_n A X L)(at level 4).
 Notation " A '[[' X ']]' n->  L  " := (array_assign_n A X L) (at level 30).
 
-Notation "'Bool_array' B '[(' X ')]' " :=(array_decl_n B X)(at level 4).
+Notation "'Bool_array' B '[(' X ')]' -> L " :=(array_decl_b B X L)(at level 4).
 Notation " B '[[' X ']]' b->  L  " := (array_assign_b B X L) (at level 30).
 
 
-Notation "'Str_array' S '[(' X ')]' " :=(array_decl_s S X)(at level 4).
+Notation "'Str_array' S '[(' X ')]' -> L " :=(array_decl_s S X L)(at level 4).
 Notation " S '[[' X ']]' s->  L  " := (array_assign_s S X L) (at level 30).
 
-Check Nat_array "x"[(2)].
-Check Bool_array "booleans"[(3)].
-Check Str_array "homework"[(2)].
+Notation " s [[' i ']] " := (elem s i)(at level 22).
+
+
+Check Nat_array "x"[(2)] -> [100 , 100].
+Check Bool_array "booleans"[(3)] -> [false, false, false].
+Check Str_array "homework"[(2)] -> ["PLP","Part1"].
 
 Check "x"[[2]] n-> [ 0, 1].
 Check "booleans"[[3]] b-> [true,true,false].
 Check "homework"[[2]] s-> ["project", "syntax"].
 
+Check "x"[['2']].
+
 
 (*Notations & examples for statements & functions *)
+
+Notation " A **" := (pointer A)(at level 30).
+Notation "&& A" := (reference A )(at level 30).
+
 Notation "'NAT' X ::= A  " := (nat_decl X A)(at level 90).
-Notation "'NAT**' X ::= A " := (ptr_nat_decl X A) (at level 90).
-
 Notation "'BOOL' X ::= A " := (bool_decl X A)(at level 90).
-Notation "'BOOL**' X ::= A " := (ptr_bool_decl X A) (at level 90).
-
 Notation "'STR' X ::= A " := (str_decl X A) (at level 90).
-Notation "'STR**' X ::= A " := (ptr_string_decl X A) (at level 90).
+
+
+
+Notation " X := 'NAT**' A " := (ptr_nat_decl X A) (at level 90).
+Notation " X := 'BOOL**' A " := (ptr_bool_decl X A) (at level 90).
+Notation " X := 'STR**' A " := (ptr_string_decl X A) (at level 90).
+Notation " X := '&&' A " := (ref_decl X A) (at level 90).
+
+
 
 Notation "'Default_nat' X" := (default_nat_decl X) (at level 90).
 Notation "'Default_bool' B" := (default_bool_decl  B) (at level 90).
 Notation "'Default_str' S" := (default_string_decl S) (at level 90).
-Notation "'Default_arr' V" := (default_array_decl V) (at level 90).
+Notation "'Default_arr' A '[(' X ')]'" := (default_array_decl A X) (at level 90).
+Notation "'Default_ptr' P" := (default_ptr_decl P) (at level 90).
 
 Notation "X n:= A" := (nat_assign X A)(at level 80).
-Notation "X n*:= A" := (ptr_nat_assign X A) (at level 90). 
-
 Notation "X b:= A" := (bool_assign X A)(at level 90).
-Notation "X b*:= A" := (ptr_bool_assign X A) (at level 90). 
-
 Notation "X s:= A" := (str_assign X A)(at level 90).
-Notation "X s*:= A" := (ptr_string_assign X A) (at level 90). 
-
+Notation "X p:= '**' A" := (ptr_assign X A)(at level 90).
+Notation "X r:= '&&' A" := (ref_assign X A)(at level 90).
 
 
 Notation "S1 ;; S2" := (sequence S1 S2) (at level 93, right associativity).
@@ -457,8 +556,8 @@ Notation "'If' '(' B ')' 'Then' '(' S1 ')' 'Else' '(' S2 ')' 'End'" := (ifthenel
 Notation "'If'(' B ) 'Then'{' S '}End'" := (ifthen B S) (at level 97).
 
 Notation "'default:{' S };" := (case_default S) (at level 97).
-Notation "'case(' X ):{ S };" := (case_x X S) (at level 97).
-Notation "'switchcase'(' Y ){ case_1 .. case_n '}'" := (switchcase Y (cons case_1 .. (cons case_n nil) .. )) (at level 98).
+Notation "'case(' X ):{ S };" := (case X S) (at level 97).
+Notation "'SWITCH' '(' Y ){ case_1 .. case_n '}'" := (switchcase Y (cons case_1 .. (cons case_n nil) .. )) (at level 40).
 
 Notation "'int' 'main()' { S }" := (main S)(at level 90). 
 Notation "'int' F (){ S }" := (function F nil S)(at level 90).
@@ -476,6 +575,9 @@ Check Default_nat "i".
 Check NAT "x" ::= 0.
 Check BOOL "y" ::= b_true.
 Check STR "s" ::= "sum".
+
+Check "ptr" **.
+Check &&"var".
 
 
 
@@ -510,16 +612,13 @@ Check
  (STR "sir2" ::= "Project") ;;
   If'( (length("sir1") ) >' (length("sir2") ) ) 
   Then'{
-       BOOL "rez" ::= ("sir1")
+       STR "rez" ::= ("sir1")
      }End.
 
-Check switchcase (0) [ case_default ("a" n:= 0) ].
- 
-(*Check switchcase ( "x")
-    { case( 5 ):{ "x" n:= 5 };
-     default :{ "x" n:= 0 }
-     }.
-*)
+
+Check SWITCH( 0 ){ default:{ "a" n:= 0 };}.
+
+
 
 
 Check
@@ -547,13 +646,15 @@ Check
     f_call "printf"(( "min" ))
   }.
 
+Check Default_ptr "NULL".
+Check  "a" := && "b".
 
-Check (NAT** "nat_pointer" ::= "n") ;; ( "n" n*:= 0)  .
-Check (BOOL** "bool_pointer" ::= "bool") ;; ( "bool" b*:= b_true).
-Check  (STR** "string_pointer" ::= "str")  ;; ("str" s*:= "syntax").
+Check ( "nat_pointer" := NAT** "n") ;; ( "n" p:= ** "p")  .
+Check ( "bool_pointer" := BOOL** "bool") ;; ( "bool" r:= && "b_true").
+Check  ( "string_pointer" := STR** "str")  ;; ("str" r:= && "syntax").
 
 Check (BOOL "new" ::= b_false) ;;
-      ( "new" b*:= b_true ) ;; ("ok" s*:= "new").
+      ( "new" p:=** "ok" ) ;; ("ok" r:= && "new").
 
 
 
